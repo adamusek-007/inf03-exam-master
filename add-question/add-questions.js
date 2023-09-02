aecb69ee31840987374bb0d3388ca0fbd7da985b
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-02 09:37:25 +0200

    - Refactored JavaScript ajax function to jQuery

--- add-question/add-questions.js
+++ add-question/add-questions.js
@@ -1,10 +1,34 @@
-var form = document.getElementById("myForm");
+$(document).ready(function () {
+  $("#form").on("submit", function (event) {
+    event.preventDefault();
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "s_insert-questions.php", true);
-  xhr.send(new FormData(form));
-  form.reset();
+    var allFieldsFilled = true;
+
+    $(this).find(":input[required]").each(function () {
+      if ($(this).val() === "") {
+        allFieldsFilled = false;
+        return false;
+      }
+    });
+
+    if (allFieldsFilled) {
+      var formData = $(this).serialize();
+
+      $.ajax({
+        type: "POST",
+        url: "s_insert-questions.php",
+        data: formData,
+        success: function (response) {
+          console.log("AJAX request complete");
+          console.log(response);
+          $("#form").reset();
+        },
+        error: function (error) {
+          console.log("AJAX request error");
+          console.log(error);
+        }
+      });
+    }
+  })
 });
 
