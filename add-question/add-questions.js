d7381b3067f5cb89623d58360bcc99e263efe6db
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-02 14:47:16 +0200

    - Extracted js to functions

--- add-question/add-questions.js
+++ add-question/add-questions.js
@@ -2,37 +2,45 @@ $(document).ready(function () {
   $("#form").on("submit", function (event) {
     event.preventDefault();
 
-    var allFieldsFilled = true;
-
-    $(this).find("textarea").each(function () {
-      if ($(this).val() === "") {
-        allFieldsFilled = false;
-        return false;
-      }
-    });
+    var allFieldsFilled = validateIncomingData();
 
     if (allFieldsFilled) {
       var formData = new FormData(this);
-
-      $.ajax({
-        type: "POST",
-        url: "s_insert-questions.php",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (response) {
-          console.log("AJAX request complete");
-          console.log(response);
-          $("#form")[0].reset();
-        },
-        error: function (error) {
-          console.log("AJAX request error");
-          console.log(error);
-        }
-      });
+      makeAjaxRequest(formData);
     } else {
-      alert ("Please fill out all required fields before submitting.");
+      alert("Please fill out all required fields before submitting.");
     }
   })
 });
 
+function makeAjaxRequest(formData) {
+  $.ajax({
+    type: "POST",
+    url: "s_insert-questions.php",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      console.log("AJAX request complete");
+      console.log(response);
+      $("#form")[0].reset();
+    },
+    error: function (error) {
+      console.log("AJAX request error");
+      console.log(error);
+    }
+  });
+}
+
+function validateIncomingData() {
+  var allFieldsFilled = true;
+  
+  $(document).find("textarea").each(function () {
+    if ($(this).val() == "") {
+      allFieldsFilled = false;
+      return false;
+    }
+  });
+
+  return allFieldsFilled;
+}
