682227ec8b41737175b4a828852d2b08d66e2cfa
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-03 08:41:54 +0200

    -Prepared file for further refafctoring

--- answer-questions/answer-questions.php
+++ answer-questions/answer-questions.php
@@ -1,39 +1,52 @@
 <?php
 include("../connection.php");
 session_start();
+class DatabaseOperator {
+    public $connection;
+    private $generating_mode;
+    private $is_mode_chosen;
+    private $query;
 
-function checkIsModeChosen()
+public function checkIsModeChosen()
 {
-    return isset($_POST["mode"]);
+    $this.$is_mode_chosen = isset($_POST["mode"]);
 }
-function getQuery($mode)
+public function getQuery()
 {
-    if ($mode == "random") {
-        return "SELECT * FROM getRandomQuestion;";
+    if ($this.$generating_mode == "random") {
+        $this.$query = "SELECT * FROM getRandomQuestion;";
     } else {
-        return "SELECT * FROM getRandomQuestion;";
-    }
-    // } else if ($mode == "worst") {
-    //     return "SELECT * FROM `questions` WHERE `u_c_answers`/`u_views` < 0.9 ORDER BY RAND() LIMIT 1;";
-    // } else if ($mode == "optimal") {
-    //     return "SELECT * FROM `questions` WHERE `u_views`!=0 AND `repetition_time` < now() ORDER BY `repetition_time` ASC LIMIT 1;";
-    // }
-}
-function getGeneratingMode($modeChosen) {
-    if ($modeChosen) {
-        return $_SESSION["mode"] = $_POST["mode"];
-    } else {
-        return $_SESSION["mode"];
+        $this.$query = "SELECT * FROM getRandomQuestion;";
     }
 }
-$modeChosen = checkIsModeChosen();
-$generating_mode = getGeneratingMode($modeChosen);
-$connection = getConnectionToDatabase();
-$query = getQuery($generating_mode);
+public function getGeneratingMode()
+{
+    if ($this.$is_mode_chosen) {
+        $this.$generating_mode = $_SESSION["mode"] = $_POST["mode"];
+    } else {
+        $this.$generating_mode =  $_SESSION["mode"];
+    }
+}
+public function getRow($result)
+{
+    return $result->fetch(PDO::FETCH_ASSOC);
+}
+public function setConnection() {
+    $this.$connection = getConnectionToDatabase();
+}
+
+};
+$db_operator = new DatabaseOperator();
+$db_operator->checkIsModeChosen();
+$db_operator->getGeneratingMode();
+$db_operator->setConnection();
+$db_operator->connection;
+$question_id = 
+
 $result = $connection->query($query);
-function getRow($result) {
-    return $result->fetch(PDO::FETCH_ASSOC);    
-}
+$row = getRow($result);
+$question_id = $row['id'];
+
 // $ans4 = htmlspecialchars(str_replace("\"", "'", $row['w_answer_3']));
 $answers_array = array();
 array_push($answers_array, $ans1, $ans2, $ans3, $ans4);
