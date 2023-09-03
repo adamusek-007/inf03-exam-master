7243788300500ed18bc8bb8d6b6f31a31dfab029
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-03 15:32:05 +0200

    - JavaScript refactoring

--- answer-questions/check-answer-and-submit.js
+++ answer-questions/check-answer-and-submit.js
@@ -1,25 +1,21 @@
-var form = document.getElementById("form");
-form.addEventListener("submit", function(event) {
+$('#myForm').submit(function (event) {
     event.preventDefault();
 });
-document.getElementById("question-details").open = true;
 
-function checkAndSubmit(userAnswerElement) {
-    var userAnswer = userAnswerElement.value;
-    var correctAnswer = document.getElementById("correct-answer").value;
-    var correctAnswerButtonXPath = "//input[@value=\""+ correctAnswer + "\"]";
-    var correctButton = document.evaluate(correctAnswerButtonXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    var questionId = document.getElementById("q-id-input").value;
-    var xhr = new XMLHttpRequest();
-        if (userAnswer == correctAnswer) {
-            userAnswerElement.style.backgroundColor = "green";
-            setTimeout(() => {  form.submit(); }, 1500);
-        } else {
-            userAnswerElement.style.backgroundColor = "#820000";
-            userAnswerElement.classList.add("opacity");
-            correctButton.style.backgroundColor = "green";
-            setTimeout(() => {  form.submit(); }, 2000);
+function checkAndSubmit(userAnswer) {
+    $.ajax({
+        type: "POST",
+        url: "s_update_question_data.php",
+        data: { usr_respo: userAnswer },
+        success: function (response) {
+            console.log(response);
+            if (response === '0') {
+                $("#form").submit();
+            } 
+            // TODO response codes and marking correct answers (MySQL and JS);
+        },
+        error: function (error) {
+            console.log(error);
         }
-    xhr.open("GET", "s_update_question_data.php?answer=" + userAnswer + "&q-id=" + questionId + "&c-answer=" + correctAnswer, true);
-    xhr.send();
+    });
 }
