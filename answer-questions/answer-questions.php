c5ef4127ebf7650b7d088240fa2e9f18380327f6
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-03 11:28:16 +0200

    - Refactored question display

--- answer-questions/answer-questions.php
+++ answer-questions/answer-questions.php
@@ -1,52 +1,35 @@
 <?php
 include("../connection.php");
-session_start();
-class DatabaseOperator {
-    public $connection;
-    private $generating_mode;
-    private $is_mode_chosen;
-    private $query;
 
-public function checkIsModeChosen()
+$q_question = "SELECT * FROM getRandomQuestion;";
+$connection = getConnectionToDatabase();
+$result = $connection->query($q_question);
+$row = $result->fetch(PDO::FETCH_ASSOC);
+$ques_content = $row['content'];
+$ques_id = $row['id'];
+$image_path = $row['image_path'];
+$q_answers = "CALL getAnswersRelatedToQuestion({$ques_id});";
+$result = $connection->query($q_answers);
+
+$answers_array = getAnswersArray($result);
+
+function getAnswersArray($result)
 {
-    $this.$is_mode_chosen = isset($_POST["mode"]);
+    $i = 0;
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $answers_array[$i] = $row['content'];
+        $i++;
+    }
+    return $answers_array;
 }
-public function getQuery()
+function randomizeAnswers($answers_array) {
+    return array_rand($answers_array);
+}
+function printAnswers($answers_array)
 {
-    if ($this.$generating_mode == "random") {
-        $this.$query = "SELECT * FROM getRandomQuestion;";
-    } else {
-        $this.$query = "SELECT * FROM getRandomQuestion;";
+    
+    foreach ($answers_array as $answer) {
+        $content = $answer->getContent();
+        echo "<input type=\"submit\" onclick=\"checkAndSubmit(this)\" name=\"user-answer\" value=\"{$content}\"><br>";
     }
 }
-public function getGeneratingMode()
-{
-    if ($this.$is_mode_chosen) {
-        $this.$generating_mode = $_SESSION["mode"] = $_POST["mode"];
-    } else {
-        $this.$generating_mode =  $_SESSION["mode"];
-    }
-}
-public function getRow($result)
-{
-    return $result->fetch(PDO::FETCH_ASSOC);
-}
-public function setConnection() {
-    $this.$connection = getConnectionToDatabase();
-}
-
-};
-$db_operator = new DatabaseOperator();
-$db_operator->checkIsModeChosen();
-$db_operator->getGeneratingMode();
-$db_operator->setConnection();
-$db_operator->connection;
-$question_id = 
-
-$result = $connection->query($query);
-$row = getRow($result);
-$question_id = $row['id'];
-
-// $ans4 = htmlspecialchars(str_replace("\"", "'", $row['w_answer_3']));
-$answers_array = array();
-array_push($answers_array, $ans1, $ans2, $ans3, $ans4);
