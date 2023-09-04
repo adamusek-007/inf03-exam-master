2c5d7dfe8cbca04818aed1c8c1d6436831f4ee5e
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-04 09:42:03 +0200

    - added chatGPT correction to this function to not display error

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ function getConnectionToDatabase(): PDO
 {
     static $pdo;
     if (!$pdo) {
-        return new PDO(
+        $pdo = new PDO(
             sprintf("mysql:host=%s;dbname=%s;charset=UTF8", DB_HOST, DB_NAME),
             DB_USER,
             DB_PASSWORD,
