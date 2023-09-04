4d5cce7267414b7df4c087c2150de2b2d15fcc5c
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-04 09:42:26 +0200

    - Refactored responding

--- answer-questions/check-answer-and-submit.js
+++ answer-questions/check-answer-and-submit.js
@@ -8,10 +8,20 @@ function checkAndSubmit(userAnswer) {
         url: "s_update_question_data.php",
         data: { usr_respo: userAnswer },
         success: function (response) {
-            console.log(response);
-            if (response === '0') {
-                $("#form").submit();
-            } 
+            var is_correct = response.substr(0,1);
+            console.log(is_correct);
+            if (is_correct == 0) {
+                var correct_answer = response.substr(1);
+                var incorrectElement = document.querySelector("input[value='" + userAnswer + "'");
+                incorrectElement.style.backgroundColor = "red";
+                var correctElement = document.querySelector("input[value='" + correct_answer + "'");
+                correctElement.style.backgroundColor = "green";
+                setTimeout( () => {$("form").submit()}, 1500);
+            } else if (is_correct == 1) {
+                var correctElement = document.querySelector("input[value='" + userAnswer + "'");
+                correctElement.style.backgroundColor = "green";
+                setTimeout( () => {$("form").submit()}, 1000);
+            }
             // TODO response codes and marking correct answers (MySQL and JS);
         },
         error: function (error) {
--- answer-questions/s_update_question_data.php
+++ answer-questions/s_update_question_data.php
@@ -2,74 +2,24 @@
 include("../connection.php");
 $answer = $_POST['usr_respo'];
 $ques_id = $_COOKIE['ques_id'];
-setcookie("ques_id","", time() -1, "/");
-$answer = str_replace("\"", '\"',$answer);
-$query = "CALL addQuestionReply({$ques_id}, \"{$answer}\");";
+setcookie("ques_id", "", time() - 1, "/");
+
+$answer = str_replace("\"", '\"', $answer);
+
 $connection = getConnectionToDatabase();
+
+$query = "CALL addUserDataAboutQuestion({$ques_id}, \"{$answer}\");";
 $connection->query($query);
-echo 0;
-
-// $u_answer = $_GET["answer"];
-// $q_id = $_GET["q-id"];
-// $c_answer = $_GET["c-answer"];
-
-// $connection = getConnectionToDatabase();
-
-// $query = "SELECT `u_views`, `u_c_answers`, `u_c_answers_streak` FROM `questions` WHERE `id`= {$q_id}";
-// $result = mysqli_query($connection, $query);
-// $row = mysqli_fetch_assoc($result);
-// $views = $row["u_views"];
-// $c_answers = $row["u_c_answers"];
-// $c_answers_streak = $row["u_c_answers_streak"];
-// $views += 1;
-// if ($u_answer == $c_answer) {
-//     $c_answers += 1;
-//     $c_answers_streak += 1;
-
-// } else {
-//     $c_answers_streak = 0;
-// }
-// $formated_date_time = getNextRepeatTime($c_answers_streak);
-// $update_query = "UPDATE `questions` SET `u_views`=\"{$views}\",`u_c_answers`=\"{$c_answers}\", `u_c_answers_streak`=\"{$c_answers_streak}\", `repetition_time`=\"{$formated_date_time}\" WHERE `id`= {$q_id};";
-// mysqli_query($connection, $update_query);
-
-// function getNextRepeatTime($c_answers_streak)
-// {
-//     $next_repeat_time = new DateTime("now");
-//     if ($c_answers_streak == 0 or $c_answers_streak == null) {
-//         $next_repeat_time->add(new DateInterval("PT1M"));
-//     } else if ($c_answers_streak == 1) {
-//         $next_repeat_time->add(new DateInterval("PT5M"));
-//     } else if ($c_answers_streak == 2) {
-//         $next_repeat_time->add(new DateInterval("PT15M"));
-//     } else if ($c_answers_streak == 3) {
-//         $next_repeat_time->add(new DateInterval("PT30M"));
-//     } else if ($c_answers_streak == 4) {
-//         $next_repeat_time->add(new DateInterval("PT1H"));
-//     } else if ($c_answers_streak == 5) {
-//         $next_repeat_time->add(new DateInterval("PT3H"));
-//     } else if ($c_answers_streak == 6) {
-//         $next_repeat_time->add(new DateInterval("PT6H"));
-//     } else if ($c_answers_streak == 7) {
-//         $next_repeat_time->add(new DateInterval("PT12H"));
-//     } else if ($c_answers_streak == 8) {
-//         $next_repeat_time->add(new DateInterval("PT16H"));
-//     } else if ($c_answers_streak == 9) {
-//         $next_repeat_time->add(new DateInterval("P2D"));
-//     } else if ($c_answers_streak == 10) {
-//         $next_repeat_time->add(new DateInterval("P3D"));
-//     } else if ($c_answers_streak == 11) {
-//         $next_repeat_time->add(new DateInterval("P1W"));
-//     } else if ($c_answers_streak == 12) {
-//         $next_repeat_time->add(new DateInterval("P2W"));
-//     } else if ($c_answers_streak == 13) {
-//         $next_repeat_time->add(new DateInterval("P1M"));
-//     } else if ($c_answers_streak == 13) {
-//         $next_repeat_time->add(new DateInterval("P3M"));
-//     }
-
-//     $formated_date_time = $next_repeat_time->format("Y-m-d H:i:s");
-//     return $formated_date_time;
-// }
-
-?>
+$query = "CALL getAnswerCorrectness({$ques_id}, \"{$answer}\");";
+$stmt = $connection->query($query);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$is_correct = $row['is_correct'];
+unset($stmt);
+echo $is_correct;
+if (!$is_correct) {
+    $query = "CALL getCorrectAnswerForQuestion({$ques_id});";
+    $stmt2 = $connection->query($query);
+    $row = $stmt2->fetch(PDO::FETCH_ASSOC);
+    $correct_answer = $row['content'];
+    echo "{$correct_answer}";
+}
