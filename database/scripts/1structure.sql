daccd04d79eafbcee97134d77e93c7fb457a9ae5
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-21 07:19:59 +0200

    Set correct database charset and collocation

--- database/scripts/1structure.sql
+++ database/scripts/1structure.sql
@@ -9,7 +9,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema egzamin_zawodowy
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `egzamin_zawodowy` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;;
+CREATE SCHEMA IF NOT EXISTS `egzamin_zawodowy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `egzamin_zawodowy` ;
 
 -- -----------------------------------------------------
