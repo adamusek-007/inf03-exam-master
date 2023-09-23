f4931e0e51e333ea89f8b77b72da8f26f5e6acc8
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-23 12:12:04 +0200

    Back to default stats

--- stats/operator.php
+++ stats/operator.php
@@ -1,10 +1,46 @@
 <?php
-$connection = get_database_connection();
-$sql = "CALL getQuestionCardsView();";
-$result = $connection->query($sql);
-while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    foreach ($row as $columnName => $columnValue) {
-        ${$columnName} = $columnValue;
+function is_get_request()
+{
+    return "GET" == $_SERVER['REQUEST_METHOD'];
+}
+function check_get_request_size()
+{
+    return sizeof($_GET);
+}
+function check_get_variable_name()
+{
+    return array_key_exists("question-id", $_GET);
+}
+function check_get_variable_data_type()
+{
+    $pattern = '/[0-9]+/';
+    return preg_match($pattern, $_GET['question-id']);
+}
+function get_questions_cards_view($connection)
+{
+    $sql = "CALL getQuestionsCardsView();";
+    $result = $connection->query($sql);
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        foreach ($row as $columnName => $columnValue) {
+            ${$columnName} = $columnValue;
+        }
+        include("questions-question-card.php");
     }
-    include("card.php");
+}
+$connection = get_database_connection();
+if (is_get_request()) {
+    $get_size = check_get_request_size();
+    if ($get_size == 1) {
+        if (check_get_variable_name()) {
+            if (check_get_variable_data_type()) {
+                get_questions_cards_view($connection);    
+            }
+        } else {
+            get_questions_cards_view($connection);
+        }
+    } else {
+        get_questions_cards_view($connection);
+    }
+} else {
+    get_questions_cards_view($connection);
 }
