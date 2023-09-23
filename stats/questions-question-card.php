6f51cee167cb7ba1dc84388fc0548214cd95c08e
adamusek-007 <adam.kuczabinski@gmail.com>
2023-09-23 11:00:27 +0200

    Renamed

--- stats/questions-question-card.php
+++ stats/questions-question-card.php
@@ -1,10 +1,10 @@
-<div class="question-card">
-    <div class="question-content"><a href="../stats/?question-id=<?=$ques_id?>"><?=$question_content?></a></div>
-    <div class="question-id">ID pytania: <?=$ques_id?></div>
-    <div class="question-last-viewed">Ostatnio wyświetlono: <?=$last_viewed?></div>
-    <section class="question-replies">
-        <div class="question-total-replies">Udzielono odpowiedzi w sumie: <?=$times_replied?></div>
-        <div class="question-correct-replies">Udzielono poprawnych odpowiedzi: <?=$correct_answers?></div>
-        <div class="question-incorrect-replies">Udzielono niepoprawnych odpowiedzi: <?=$incorrect_answers?></div>
+<div class="questions-question-card">
+    <div class="questions-question-content"><a href="../stats/?question-id=<?=$ques_id?>"><?=$question_content?></a></div>
+    <div class="questions-question-id">ID pytania: <?=$ques_id?></div>
+    <div class="questions-question-last-viewed">Ostatnio wyświetlono: <?=$last_viewed?></div>
+    <section class="questions-question-replies">
+        <div class="questions-question-total-replies">Udzielono odpowiedzi w sumie: <?=$times_replied?></div>
+        <div class="questions-question-correct-replies">Udzielono poprawnych odpowiedzi: <?=$correct_answers?></div>
+        <div class="questions-question-incorrect-replies">Udzielono niepoprawnych odpowiedzi: <?=$incorrect_answers?></div>
     </section>
 </div>
