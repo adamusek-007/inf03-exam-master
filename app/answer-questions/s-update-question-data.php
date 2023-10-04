4f40b646da4df68f0b4e6cf192d996f7782d01e7
adamusek-007 <adam.kuczabinski@gmail.com>
2023-10-04 15:20:43 +0200

    Refactor question data update script

--- app/answer-questions/s-update-question-data.php
+++ app/answer-questions/s-update-question-data.php
@@ -1,25 +1,49 @@
 <?php
 include("../database/connection.php");
-$answer = $_POST['usr_respo'];
-$ques_id = $_COOKIE['ques_id'];
-setcookie("ques_id", "", time() - 1, "/");
+class QuestionReplyProcessor
+{
+    private string $user_reply_answer;
+    private int $question_id;
+    private bool $is_reply_correct;
+    private string $correct_answer;
 
-$answer = str_replace("\"", '\"', $answer);
+    function addQuestionReply()
+    {
+        $connection = get_database_connection();
+        $query = "CALL addQuestionReply({$this->question_id}, \"{$this->user_reply_answer}\");";
+        $connection->query($query);
+    }
+    function getAnswerCorrectness(): bool
+    {
+        $connection = get_database_connection();
+        $query = "CALL getAnswerCorrectness({$this->question_id}, \"{$this->user_reply_answer}\");";
+        $row = $connection->query($query)->fetch(PDO::FETCH_ASSOC);
+        return $row['is_correct'];
+    }
+    function getCorrectAnswer()
+    {
+        $connection = get_database_connection();
+        $query = "CALL getQuestionCorrectAnswer({$this->question_id});";
+        $row = $connection->query($query)->fetch(PDO::FETCH_ASSOC);
+        $correct_answer = $row['content'];
+        $this->correct_answer = $correct_answer;
+    }
 
-$connection = get_database_connection();
+    function unsetCookie()
+    {
+        setcookie("question_id", "", time() - 1);
+    }
 
-$query = "CALL addQuestionReply({$ques_id}, \"{$answer}\");";
-$connection->query($query);
-$query = "CALL getAnswerCorrectness({$ques_id}, \"{$answer}\");";
-$stmt = $connection->query($query);
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-$is_correct = $row['is_correct'];
-unset($stmt);
-echo $is_correct;
-if (!$is_correct) {
-    $query = "CALL getQuestionCorrectAnswer({$ques_id});";
-    $stmt2 = $connection->query($query);
-    $row = $stmt2->fetch(PDO::FETCH_ASSOC);
-    $correct_answer = $row['content'];
-    echo "{$correct_answer}";
+    function __construct()
+    {
+        $this->user_reply_answer = str_replace("\"", '\"', $_POST['user_reply_answer']);
+        $this->question_id = intval($_COOKIE['question_id']);
+        $this->unsetCookie();
+        $this->addQuestionReply();
+        $this->is_reply_correct = $this->getAnswerCorrectness();
+        $this->getCorrectAnswer();
+        $json = json_encode(array("reply_correctness" => $this->is_reply_correct, "correct_answer" => $this->correct_answer));
+        echo $json;
+    }
 }
+new QuestionReplyProcessor();
