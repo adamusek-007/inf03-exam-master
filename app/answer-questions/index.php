5d7040ac11763ac61d52e6e22d49a7abe7e355e4
adamusek-007 <adam.kuczabinski@gmail.com>
2023-10-04 11:17:03 +0200

    Change index to view on index include view

--- app/answer-questions/index.php
+++ app/answer-questions/index.php
@@ -1,34 +1,3 @@
 ﻿<?php
-include("answer-questions.php");
-?>
-<!DOCTYPE html>
-<html lang="pl-PL">
-
-<head>
-    <title>Odpowiadaj</title>
-    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../resources/styles/answer-question.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-</head>
-
-<body>
-    <form id="form" action="./" method="post" class="first-site-form">
-        <label for="user-answer">
-            <?=htmlspecialchars($ques_content)?>
-        </label>
-        <?=print_image($image_path)?>
-        <fieldset id="user-answer">
-            <legend>Wybierz odpowiedź</legend>
-            <?=print_answers($answers_array)?>
-            <input type="button" onclick="(checkAndSubmit(this.value))" name="user-answer" value="Nie wiem">
-        </fieldset>
-    </form>
-    <details>
-        <p>Id pytania: <?=$ques_id?></p>
-    </details>
-    <script src="check-answer-and-submit.js"></script>
-</body>
-
-</html>
+include ("./controller.php");
+include ("./view.php");
---
+++ app/answer-questions/view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+
+<head>
+    <title>Odpowiadaj</title>
+    <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../resources/styles/answer-question.css">
+</head>
+
+<body>
+    <form id="form" action="./" method="post" class="first-site-form">
+        <label for="user-answer">
+            <?=$question->get_content()?>
+        </label>
+        <?=$question->print_image()?>
+        <fieldset id="user-answer">
+            <legend>Wybierz odpowiedź</legend>
+            <?php $answers->print()?>
+        </fieldset>
+    </form>
+    <details>
+        <p>Id pytania: <?=$question->get_id()?></p>
+    </details>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="check-answer-and-submit.js"></script>
+</body>
+
+</html>
