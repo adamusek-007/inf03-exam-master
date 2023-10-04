b32337ee53696552413cf38251e9211d3c2cd7ca
adamusek-007 <adam.kuczabinski@gmail.com>
2023-10-04 17:54:19 +0200

    Rename function names to match other

--- app/answer-questions/s-update-question-data.php
+++ app/answer-questions/s-update-question-data.php
@@ -7,20 +7,20 @@ class QuestionReplyProcessor
     private bool $is_reply_correct;
     private string $correct_answer;
 
-    function addQuestionReply()
+    function add_question_reply()
     {
         $connection = get_database_connection();
         $query = "CALL addQuestionReply({$this->question_id}, \"{$this->user_reply_answer}\");";
         $connection->query($query);
     }
-    function getAnswerCorrectness(): bool
+    function get_answer_correctness(): bool
     {
         $connection = get_database_connection();
         $query = "CALL getAnswerCorrectness({$this->question_id}, \"{$this->user_reply_answer}\");";
         $row = $connection->query($query)->fetch(PDO::FETCH_ASSOC);
         return $row['is_correct'];
     }
-    function getCorrectAnswer()
+    function get_correct_answer()
     {
         $connection = get_database_connection();
         $query = "CALL getQuestionCorrectAnswer({$this->question_id});";
@@ -29,7 +29,7 @@ class QuestionReplyProcessor
         $this->correct_answer = $correct_answer;
     }
 
-    function unsetCookie()
+    function unset_cookie()
     {
         setcookie("question_id", "", time() - 1);
     }
@@ -38,10 +38,10 @@ class QuestionReplyProcessor
     {
         $this->user_reply_answer = str_replace("\"", '\"', $_POST['user_reply_answer']);
         $this->question_id = intval($_COOKIE['question_id']);
-        $this->unsetCookie();
-        $this->addQuestionReply();
-        $this->is_reply_correct = $this->getAnswerCorrectness();
-        $this->getCorrectAnswer();
+        $this->unset_cookie();
+        $this->add_question_reply();
+        $this->is_reply_correct = $this->get_answer_correctness();
+        $this->get_correct_answer();
         $json = json_encode(array("reply_correctness" => $this->is_reply_correct, "correct_answer" => $this->correct_answer));
         echo $json;
     }
