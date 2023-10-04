cf1473ef21ea0f02908b79ec608340fc124d16ba
adamusek-007 <adam.kuczabinski@gmail.com>
2023-10-04 13:10:05 +0200

    Refactor of validate script, divide into functions

--- app/answer-questions/check-answer-and-submit.js
+++ app/answer-questions/check-answer-and-submit.js
@@ -1,31 +1,41 @@
-$('#myForm').submit(function (event) {
-    event.preventDefault();
-});
-
-function checkAndSubmit(userAnswer) {
+function checkAndSubmit(userReplyAnswer) {
     $.ajax({
         type: "POST",
         url: "s-update-question-data.php",
-        data: { usr_respo: userAnswer },
-        success: function (response) {
-            var is_correct = response.substr(0,1);
-            console.log(is_correct);
-            if (is_correct == 0) {
-                var correct_answer = response.substr(1);
-                var incorrectElement = document.querySelector("input[value='" + userAnswer + "'");
-                incorrectElement.style.backgroundColor = "red";
-                var correctElement = document.querySelector("input[value='" + correct_answer + "'");
-                correctElement.style.backgroundColor = "green";
-                setTimeout( () => {$("form").submit()}, 1500);
-            } else if (is_correct == 1) {
-                var correctElement = document.querySelector("input[value='" + userAnswer + "'");
-                correctElement.style.backgroundColor = "green";
-                setTimeout( () => {$("form").submit()}, 1000);
-            }
-            // TODO response codes and marking correct answers (MySQL and JS);
-        },
-        error: function (error) {
-            console.log(error);
-        }
+        data: { user_reply_answer: userReplyAnswer },
+        success: (response) => { handleAjaxResponse(response, userReplyAnswer) },
+        error: (error) => { alert(error) }
     });
+}
+function handleAjaxResponse(response, userReplyAnswer) {
+    var is_correct = getReplyCorrectness(response);
+    if (is_correct == 1) {
+        replySuccess(userReplyAnswer);
+    } else {
+        replyFailure(response, userReplyAnswer)
+    }
+}
+function replySuccess(userReplyAnswer) {
+    setElementBackground(userReplyAnswer, "green");
+    submitForm(1000);
+}
+
+function replyFailure(response, userReplyAnswer) {
+    setElementBackground(userReplyAnswer, "red");
+    var correct_answer = response.substr(1);
+    setElementBackground(correct_answer, "green");
+    submitForm(1500);
+}
+
+function submitForm(timeout) {
+    setTimeout(() => { $("#form").submit() }, timeout);
+}
+
+function setElementBackground(value, color) {
+    var element = document.querySelector("input[value='" + value + "'");
+    element.style.backgroundColor = color;
+}
+
+function getReplyCorrectness(response) {
+    return response.substr(0, 1);
 }
