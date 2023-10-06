d7812beb57ea1062402a0e2e5fbf815d4bb62aa2
adamusek-007 <adam.kuczabinski@gmail.com>
2023-10-06 07:55:05 +0200

    Refactor question inserting

--- app/add-question/s-insert-questions.php
+++ app/add-question/s-insert-questions.php
@@ -1,12 +1,23 @@
 <?php
 include("../database/connection.php");
-class QuestionInserter
-{
-    public static array $form_fields = ["content", "c-answer", "w-answer-1", "w-answer-2", "w-answer-3"];
-    public static array $supported_file_types = ["image/jpeg", "image/jpg", "image/png"];
-    public static string $file_upload_dir = "../resources/images/";
+class Form {
+    public static array $expected_fields_names = ["content", "c-answer", "w-answer-1", "w-answer-2", "w-answer-3"];
 
+}
+class Image {
+    public static array $supported_file_types = ["image/jpeg", "image/jpg", "image/png"];
+    private bool|null $is_attached = null;
+    private bool|null $is_type_correct = null;
+    function __construct() {
+        
+    }
+}
+class FormValidator
+{
     private bool $data_completition;
+    private bool|null $is_image_attached = null;
+
+    private bool|null $is_image_type_correct = null;
 
     function set_data_completition(bool $data_complete)
     {
@@ -16,53 +27,81 @@ class QuestionInserter
     {
         return $this->data_completition;
     }
-
+    function set_is_image_attached(bool $is_image_attached)
+    {
+        $this->is_image_attached = $is_image_attached;
+    }
+    function get_image_attachness(): bool
+    {
+        return $this->is_image_attached;
+    }
+    function set_is_image_type_correct(bool $is_image_type_correct)
+    {
+        $this->is_image_type_correct = $is_image_type_correct;
+    }
+    function get_is_image_type_correct(): bool
+    {
+        return $this->is_image_type_correct;
+    }
     function check_data_completion()
     {
-        foreach (QuestionInserter::$form_fields as $field_name) {
-            if (array_key_exists($field_name, $_POST)) {
-                if (!isset($_POST[$field_name]) || empty($_POST[$field_name])) {
-                    $this->set_data_completition(false);
-                    break;
-                }
-            } else {
+        foreach (Form::$expected_fields_names as $field_name) {
+            if (!isset($_POST[$field_name]) || empty($_POST[$field_name])) {
                 $this->set_data_completition(false);
+                return;
             }
         }
         $this->set_data_completition(true);
     }
-
-    function check_image_attachment(): bool
+    function check_is_image_attached()
     {
-        if (array_key_exists("image", $_FILES) && array_key_exists("name", $_FILES["image"]) && $_FILES["image"]["type"] != "") {
-            return true;
-        }
-        return false;
+        $this->set_is_image_attached(
+            array_key_exists("image", $_FILES) &&
+            array_key_exists("type", $_FILES["image"]) &&
+            $_FILES["image"]["type"] !== ""
+        );
     }
-    function check_image_type_correctness(): bool
+    function check_is_image_type_correct()
     {
         $uploaded_file_type = $_FILES["image"]["type"];
-        return in_array($uploaded_file_type, QuestionInserter::$supported_file_types);
+        $this->set_is_image_type_correct(in_array($uploaded_file_type, Image::$supported_file_types));
     }
-
     function __construct()
     {
         $this->check_data_completion();
-        if ($this->data_completition) {
-            if ($this->check_image_attachment()) {
-                if ($this->check_image_type_correctness()) {
-                    $this->upload_image();
-                    $this->insert_data(true);
-                } else {
-                    echo "Typ załączonego obrazu nie jest obsługiwany. \n Obsługiwane typy plików to: PNG, JPEG, JPG.";
-                    exit;
-                }
-            } else {
-                $this->insert_data(false);
+        if ($this->get_data_completition()) {
+            $this->check_is_image_attached();
+            if ($this->get_image_attachness()) {
+                $this->check_is_image_type_correct();
             }
+        }
+    }
+
+}
+class QuestionInserter
+{
+    public static string $file_upload_dir = "../resources/images/";
+
+    function __construct(FormValidator $form_validator)
+    {
+        if ($form_validator->get_data_completition()) {
+            $this->internal_proceed($form_validator);
         } else {
             echo "Wymagane pola nie są wypełnione.";
             exit;
+        }
+    }
+    function internal_proceed($form_validator)
+    {
+        if ($form_validator->get_image_attachness()) {
+            if ($form_validator->get_is_image_type_correct()) {
+                $this->upload_image();
+                $this->insert_data(true);
+            } else {
+                echo "Typ załączonego obrazu nie jest obsługiwany. \n Obsługiwane typy plików to: PNG, JPEG, JPG.";
+            }
+        } else {
+            $this->insert_data(false);
         }
     }
     function upload_image()
@@ -76,12 +115,9 @@ class QuestionInserter
         try {
             $connection = get_database_connection();
 
-            $q_question_insert = $this->get_question_insert_query($has_image);
-            $connection->query($q_question_insert);
-
+            $connection->query($this->get_question_insert_query($has_image));
             $question_id = $this->get_latest_question_id();
-            $q_answers_insert = $this->get_answers_insert_query($question_id);
-            $connection->query($q_answers_insert);
+            $connection->query($this->get_answers_insert_query($question_id));
             $response = array(
                 "status" => "success",
                 "message" => "Question successfully inserted."
@@ -92,7 +128,6 @@ class QuestionInserter
                 "message" => "An error occurred. Please try again later."
             );
         }
-        // NOT SURE aobut header
         header('Content-Type: application/json');
         echo json_encode($response);
     }
@@ -123,10 +158,11 @@ class QuestionInserter
     function get_latest_question_id()
     {
         $connection = get_database_connection();
-        $result = $connection->query("CALL getLatestAddedQuestionId();");
-        $row = $result->fetch(PDO::FETCH_ASSOC);
+        $row = $connection->query("CALL getLatestAddedQuestionId();")->fetch(PDO::FETCH_ASSOC);
         return intval($row['id']);
     }
 }
-new QuestionInserter();
+// $form_data = new FormData();
+$form_validator = new FormValidator();
+new QuestionInserter($form_validator);
 ?>
