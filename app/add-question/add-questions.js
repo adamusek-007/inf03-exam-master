c72bd00b62ddcd9586a54703314a1596c66832fd
adamusek-007 <adam.kuczabinski@gmail.com>
2023-11-11 15:34:19 +0100

    Make code OOP and SOLID

--- app/add-question/add-questions.js
+++ app/add-question/add-questions.js
@@ -1,85 +1,100 @@
-const form = document.querySelector("form");
+class FormHandler {
+  constructor(formSelector, messageBoxSelector) {
+    this.form = document.querySelector(formSelector);
+    this.messageBox = document.querySelector(messageBoxSelector);
 
-if (form !== null) {
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    handleFormSubmit(form);
-  });
-}
-
-function handleFormSubmit(form) {
-  if (validateRequiredFieldsData(form)) {
-    makeAjaxRequest(new FormData(form));
-  } else {
-    displayMessage("error", "Wymagane pola nie są wypełnione.");
-  }
-}
-
-function validateRequiredFieldsData(form) {
-  const fields = form.querySelectorAll("textarea");
-  for (const field of fields) {
-    if (field.value === "") {
-      return false;
+    if (this.form !== null) {
+      this.form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        this.handleFormSubmit();
+      });
     }
   }
-  return true;
-}
 
-function displayMessage(status, messageContent) {
-  const messageBox = document.querySelector("#message-box");
-
-  if (messageBox !== null) {
-    const className = "message-" + status;
-    messageBox.classList.add(className);
-    messageBox.innerHTML = messageContent;
-
-    setTimeout(() => {
-      clearMessageBox(messageBox, className);
-    }, 1500);
+  handleFormSubmit() {
+    if (this.validateRequiredFieldsData()) {
+      this.makeAjaxRequest(new FormData(this.form));
+    } else {
+      this.displayMessage("error", "Wymagane pola nie są wypełnione.");
+    }
   }
-}
 
-function clearMessageBox(messageBox, className) {
-  messageBox.innerHTML = "";
-  messageBox.classList.remove(className);
-}
-
-function makeAjaxRequest(formData) {
-  const xhttp = new XMLHttpRequest();
-  const type = "POST";
-  const url = "question-add-controller.php";
-  xhttp.open(type, url, true);
-
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4) {
-      if (this.status == 200) {
-        handleAjaxResponse(this.response);
-      } else {
-        handleAjaxError(this.statusText);
+  validateRequiredFieldsData() {
+    const fields = this.form.querySelectorAll("textarea");
+    for (const field of fields) {
+      if (field.value === "") {
+        return false;
       }
     }
-  };
+    return true;
+  }
 
-  xhttp.send(formData);
-}
+  displayMessage(status, messageContent) {
+    if (this.messageBox !== null) {
+      const className = "message-" + status;
+      this.messageBox.classList.add(className);
+      this.messageBox.innerHTML = messageContent;
 
-function handleAjaxError(error) {
-  displayMessage("error", "There was an error: " + error);
-}
+      setTimeout(() => {
+        this.clearMessageBox(className);
+      }, 1500);
+    }
+  }
 
-function handleAjaxResponse(textResponse) {
-  const response = JSON.parse(textResponse);
-  displayMessage(response.status, response.message);
+  clearMessageBox(className) {
+    this.messageBox.innerHTML = "";
+    this.messageBox.classList.remove(className);
+  }
 
-  if (response.status) {
-    resetForm();
+  makeAjaxRequest(formData) {
+    const ajaxRequest = new AjaxRequest();
+    ajaxRequest.send(
+      formData,
+      (response) => this.handleAjaxResponse(response),
+      (error) => this.handleAjaxError(error)
+    );
+  }
+
+  handleAjaxError(error) {
+    this.displayMessage("error", "There was an error: " + error);
+  }
+
+  handleAjaxResponse(textResponse) {
+    const response = JSON.parse(textResponse);
+    this.displayMessage(response.status, response.message);
+
+    if (response.status) {
+      this.resetForm();
+    }
+  }
+
+  resetForm() {
+    if (this.form !== null) {
+      this.form.reset();
+    }
   }
 }
 
-function resetForm() {
-  const form = document.querySelector("form");
+class AjaxRequest {
+  send(formData, successCallback, errorCallback) {
+    const xhttp = new XMLHttpRequest();
+    const type = "POST";
+    const url = "question-add-controller.php";
 
-  if (form !== null) {
-    form.reset();
+    xhttp.open(type, url, true);
+
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          successCallback(this.response);
+        } else {
+          errorCallback(this.statusText);
+        }
+      }
+    };
+
+    xhttp.send(formData);
   }
 }
+
+const formHandler = new FormHandler("form", "#message-box");
