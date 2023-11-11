9e22b5ad13a5a5167d1972a40026bad0116dcdaf
adamusek-007 <adam.kuczabinski@gmail.com>
2023-11-11 12:00:59 +0100

    Update stats files inclusion

--- app/stats/index.php
+++ app/stats/index.php
@@ -1,5 +1,13 @@
 <?php
 include("../database/connection.php");
+include("./src/Answer.php");
+include("./src/QuestionCardBottom.php");
+include("./src/QuestionCardMid.php");
+include("./src/QuestionCardsView.php");
+include("./src/QuestionCardView.php");
+include("./src/QuestionTopCard.php");
+include("./src/Reply.php");
+include("./src/ViewGenerator.php");
 include("controller.php");
 ?>
 <link rel="stylesheet" href="../resources/styles/stats.css">
