7b603577009d6fbec116b9bc6e426de7fcc628bc
adamusek-007 <adam.kuczabinski@gmail.com>
2023-11-11 15:13:54 +0100

    Change jQuery to normal javascript

--- app/add-question/add-questions.js
+++ app/add-question/add-questions.js
@@ -1,62 +1,78 @@
-$(document).ready(function () {
-  $("#form").on("submit", function (event) {
+let form = document.querySelector("form");
+
+if (form !== null) {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (validateRequiredFieldsData()) {
-      makeAjaxRequest(new FormData(this));
-    } else {
-      displayMessage("error", "Wymagane pola nie są wypełnione.");
+    handleFormSubmit(form);
+  });
+}
+
+function handleFormSubmit(form) {
+  if (validateRequiredFieldsData(form)) {
+    makeAjaxRequest(new FormData(form));
+  } else {
+    displayMessage("error", "Wymagane pola nie są wypełnione.");
+  }
+}
+
+function validateRequiredFieldsData(form) {
+  let isValid = true;
+  let fields = form.querySelectorAll("textarea");
+  fields.forEach((field) => {
+    if (field.value === "") {
+      isValid = false;
+      return false;
     }
   });
+  return isValid;
+}
 
-  function makeAjaxRequest(formData) {
-    $.ajax({
-      type: "POST",
-      url: "question-add-controller.php",
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: handleAjaxResponse,
-      error: handleAjaxError,
-    });
+function displayMessage(status, messageContent) {
+  let messageBox = document.querySelector("#message-box");
+  if (messageBox != null) {
+    let className = "message-" + status;
+    messageBox.classList.add(className);
+    messageBox.innerHTML = messageContent;
+    setTimeout(() => {
+      clearMessageBox(messageBox, className);
+    }, 1500);
   }
+}
 
-  function handleAjaxResponse(textResponse) {
-    response = JSON.parse(textResponse);
-    displayMessage(response.status, response.message);
-    if (response.status) {
-      resetForm();
+function clearMessageBox(messageBox, className) {
+  messageBox.innerHTML = "";
+  messageBox.classList.remove(className);
+}
+
+function makeAjaxRequest(formData) {
+  let xhttp = new XMLHttpRequest();
+  let type = "POST";
+  let url = "question-add-controller.php";
+  xhttp.open(type, url, true);
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      handleAjaxResponse(this.response);
+    } else {
+      handleAjaxError(this.statusText);
     }
-  }
+  };
+  xhttp.send(formData);
+}
 
-  function handleAjaxError(error) {
-    displayMessage("error", "There was an error: " + error.statusText);
-  }
+function handleAjaxError(error) {
+  displayMessage("error", "There was an error: " + error);
+}
 
-  function resetForm() {
-    $("#form")[0].reset();
+function handleAjaxResponse(textResponse) {
+  let response = JSON.parse(textResponse);
+  displayMessage(response.status, response.message);
+  if (response.status) {
+    resetForm();
   }
+}
 
-  function displayMessage(status, messageContent) {
-    $("#message-box").addClass("message-" + status);
-    $("#message-box").text(messageContent)
-    setTimeout(clearMessageBox, 1500);
-  }
-
-  function clearMessageBox() {
-    $("#message-box").text("");
-    $("#message-box").removeClass();
-  }
-
-  function validateRequiredFieldsData() {
-    let isValid = true;
-    $(document)
-      .find("textarea")
-      .each(function () {
-        if ($(this).val() === "") {
-          isValid = false;
-          return false;
-        }
-      });
-    return isValid;
-  }
-});
+function resetForm() {
+  let form = document.querySelector("form");
+  if (form != null) form.reset();
+}
